111e71c17754734c2bb24bad44df309b3d53eb48
ahraman <ahraman.programming@gmail.com>
2026-07-10 18:50:23 +0330

    feat(atlas): impl essentials for server logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
 pub mod error;
 
+use axum::{Router, response::IntoResponse, routing::get};
+use tokio::net::TcpListener;
+
 pub use self::error::{Error, Result};
 
 pub async fn run() -> Result<()> {
-    println!("Hello, world!");
-    Ok(())
+    let _ = dotenvy::dotenv()?;
+
+    let router = Router::new().route("/", get(root));
+
+    let server_url =
+        &std::env::var("SERVER_URL").map_err(|e| Error::Env("SERVER_URL".to_string(), e))?;
+    let listener = TcpListener::bind(server_url).await?;
+
+    Ok(axum::serve(listener, router).await?)
+}
+
+async fn root() -> impl IntoResponse {
+    "Hello, world!"
 }
