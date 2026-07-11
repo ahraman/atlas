297b5a56c9d38339d20a70df9aeb3e17624a0ea5
ahraman <ahraman.programming@gmail.com>
2026-07-11 20:44:46 +0330

    feat(web): use new template to render HTML output

--- src/web/routes.rs
+++ src/web/routes.rs
@@ -1,7 +1,18 @@
-use axum::{Router, response::IntoResponse, routing::get};
+use askama::Template;
+use axum::{
+    Router, debug_handler,
+    response::{Html, IntoResponse},
+    routing::get,
+};
 use tower_http::services::ServeDir;
 
-use crate::web::AppState;
+use crate::{
+    Result,
+    web::{
+        AppState,
+        templates::{PageInfo, PageViewTemplate, SiteInfo},
+    },
+};
 
 impl super::Server {
     pub(super) fn build_router(&self, state: AppState) -> Router {
@@ -12,6 +23,13 @@ impl super::Server {
     }
 }
 
-async fn root() -> impl IntoResponse {
-    "Hello, world!"
+#[debug_handler(state = AppState)]
+async fn root(AppState(app): AppState) -> Result<impl IntoResponse> {
+    Ok(Html::from(
+        PageViewTemplate::new(
+            SiteInfo::new(&app.config.site_name),
+            PageInfo::new("root", "<p>Hello, world!</p>"),
+        )
+        .render()?,
+    ))
 }
