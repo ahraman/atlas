875775e2b6c321321b98d6b1d87e21fe5bfc6408
ahraman <ahraman.programming@gmail.com>
2026-07-11 12:57:52 +0330

    feat(web): normalize request paths by trimming trailing slashes and add connect info to requests

--- src/web/server.rs
+++ src/web/server.rs
@@ -1,6 +1,9 @@
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
 
+use axum::{ServiceExt, extract::Request};
 use tokio::net::TcpListener;
+use tower::Layer;
+use tower_http::normalize_path::NormalizePathLayer;
 
 use crate::{Result, app::App, web::AppState};
 
@@ -14,11 +17,14 @@ impl Server {
 
     pub async fn run(self, app: Arc<App>) -> Result<()> {
         let router = self.build_router(AppState::new(app.clone()));
+        let middleware = NormalizePathLayer::trim_trailing_slash().layer(router);
+        let service =
+            ServiceExt::<Request>::into_make_service_with_connect_info::<SocketAddr>(middleware);
 
         let server_url = &app.config.server_url;
         let listener = TcpListener::bind(server_url).await?;
         tracing::info!("listening at: {server_url}");
 
-        Ok(axum::serve(listener, router).await?)
+        Ok(axum::serve(listener, service).await?)
     }
 }
